846cd0b2919d7f1adee5eee91e6dfea02916ace1
Shivam shrivastava <shivam091204@gmail.com>
2024-09-05 10:13:16 +0530

    first commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants{
+  static const String databaseID = "66d93074003c519e942d";
+  static const String projectID = "66d92c4300034c613e24";
+  static const String endPoint = 'https://localhost:3000/v1';
+}
